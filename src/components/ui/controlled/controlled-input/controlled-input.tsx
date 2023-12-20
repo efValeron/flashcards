3abc460e626aca0300a47da4feaf933f31e6c2bc
efValeron <masterbox06@gmail.com>
2023-12-20 13:33:21 +0600

    feat: add controlled input

---
+++ src/components/ui/controlled/controlled-input/controlled-input.tsx
@@ -0,0 +1,21 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Input, InputProps } from '@/components/ui/input'
+
+type ControlledTextFieldProps<T extends FieldValues> = UseControllerProps<T> &
+  Omit<InputProps, 'id' | 'onChange' | 'value'>
+
+export const ControlledInput = <T extends FieldValues>(props: ControlledTextFieldProps<T>) => {
+  const { control, defaultValue, name, rules, shouldUnregister, ...rest } = props
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    defaultValue,
+    name,
+    rules,
+    shouldUnregister,
+  })
+
+  return <Input id={name} onChange={onChange} value={value} {...rest} />
+}
